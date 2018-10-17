a29d9fe835f81b446ddc9b9b5d047267aaffb793
Harry Chou <yarram1997@gmail.com>
2018-10-17 23:39:56 +0800

    Reverse Integer

---
+++ problems/String/reverse/reverse.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Harry Chou at 2018/10/17.
+ * Description: Given a 32-bit signed integer, reverse digits of an integer. And
+ * returns 0 when the reversed integer overflows.
+ */
+public class reverse {
+    public int reverseStr(int x) {
+        int result = 0;
+        while (x != 0) {
+            int temp = result * 10 + x % 10;
+            if (temp / 10 != result) {
+                return 0;
+            }
+            result = temp;
+            x /= 10;
+        }
+        return result;
+    }
+}
