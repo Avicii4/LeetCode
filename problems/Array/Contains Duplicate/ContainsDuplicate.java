a606582619b52d0a6f1d3da3139bb488b92593ec
Harry Chou <yarram1997@gmail.com>
2018-10-20 20:15:02 +0800

    Contains Dulipcates

--- problems/Array/Contains Duplicate/ContainsDuplicate.java
+++ problems/Array/Contains Duplicate/ContainsDuplicate.java
@@ -2,8 +2,8 @@
  * Created by Harry Chou at 2018/11/13.
  * Description: Given an array of integers,find if the array contains any duplicates.
  */
-public class containsDuplicate {
-    public boolean find(int[] nums) {
+public class ContainsDuplicate {
+    public boolean findDuplicate(int[] nums) {
         for (int i = 0; i < nums.length; i++)
             for (int j = 1; j < nums.length - i; j++) {
                 if (nums[i] == nums[i + j])
