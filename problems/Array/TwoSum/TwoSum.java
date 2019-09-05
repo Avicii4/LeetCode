c9490aab46618115a5196d13b21f46542484707e
Harry Chou <yarram1997@gmail.com>
2019-09-05 20:37:19 +0800

    Two Sum Update

--- problems/Array/TwoSum/TwoSum.java
+++ problems/Array/TwoSum/TwoSum.java
@@ -15,4 +15,17 @@ public class TwoSum {
                     }
         return result;
     }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int anotherNum = target - nums[i];
+            if (map.containsKey(anotherNum) && map.get(anotherNum) != i) {
+                return new int[]{map.get(anotherNum), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
 }
