d31c863a93039ac29d6da2f2371846db4807b820
Avicii4 <iharrychou@gmail.com>
2023-07-16 15:03:05 +0800

    update

--- Python/test/Sorting.py
+++ Python/test/Sorting.py
@@ -16,12 +16,20 @@ def bubble_sort(nums: List[int]):
 
 def insertion_sort(nums: List[int]):
     for i in range(1, len(nums)):
-        tmp = nums[i]
+        tmp = nums[i]  # 要插进前面的数
         j = i - 1
-        while j >= 0 and nums[j] >= tmp:
+        while j >= 0 and nums[j] >= tmp:  # 若前面的数更大，则挪后面去
             nums[j + 1] = nums[j]
             j -= 1
-        nums[j + 1] = tmp
+        nums[j + 1] = tmp  # 此时位置合适，插入 tmp
+
+
+def insertion_sort_2(nums: List[int]):
+    for i in range(1, len(nums)):
+        for j in range(i - 1, -1, -1):
+            if nums[j + 1] < nums[j]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
+
 
 def selection_sort(nums: List[int]):
     n = len(nums)
@@ -39,7 +47,7 @@ if __name__ == '__main__':
         for _ in range(n):
             a.append(randint(0, 1000))
         a_sort = sorted(a)
-        selection_sort(a)
+        insertion_sort_2(a)
         if not a == a_sort:
             print(a)
             print(a_sort)
