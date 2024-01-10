04d3e9272beaccc124f824d056ba20f2ba340183
Avicii4 <iharrychou@gmail.com>
2024-01-10 20:54:15 +0800

    merge sort

--- Python/test/Sorting.py
+++ Python/test/Sorting.py
@@ -41,14 +41,53 @@ def selection_sort(nums: List[int]):
         nums[i], nums[tmp] = nums[tmp], nums[i]
 
 
+def merge_sort(nums: List[int]):
+    if nums is None or len(nums) < 2:
+        return
+    process(nums, 0, len(nums) - 1)
+
+
+def process(nums, left, right):
+    if left == right:
+        return
+    mid = (left + right) >> 1
+    process(nums, left, mid)
+    process(nums, mid + 1, right)
+    merge_both(nums, left, mid, right)
+
+
+def merge_both(nums, left, mid, right):
+    help = [0 for _ in range(right - left + 1)]
+    p, q = left, mid + 1  # 指向两部分数组各自的最右侧
+    cur = 0
+    while p <= mid and q <= right:
+        if nums[p] <= nums[q]:
+            help[cur] = nums[p]
+            p += 1
+        else:
+            help[cur] = nums[q]
+            q += 1
+        cur += 1
+    while p <= mid:
+        help[cur] = nums[p]
+        cur += 1
+        p += 1
+    while q <= right:
+        help[cur] = nums[q]
+        cur += 1
+        q += 1
+    for i in range(len(help)):
+        nums[left + i] = help[i]
+
+
 if __name__ == '__main__':
     # 用户对数生成器测试
-    for _ in range(100):
+    for _ in range(1000):
         n, a = randint(1, 100), []
         for _ in range(n):
             a.append(randint(0, 1000))
         a_sort = sorted(a)
-        insertion_sort_2(a)
+        merge_sort(a)
         if not a == a_sort:
             print(a)
             print(a_sort)
